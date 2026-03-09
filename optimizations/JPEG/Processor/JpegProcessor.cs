91efa1ff64341a59ec5a994699d8f179654e691d
maks march <1maks_2055@mail.ru>
2026-03-10 00:08:23 +0500

    fixed

--- optimizations/JPEG/Processor/JpegProcessor.cs
+++ optimizations/JPEG/Processor/JpegProcessor.cs
@@ -14,6 +14,18 @@ public class JpegProcessor : IJpegProcessor
 	public const int CompressionQuality = 70;
 	private const int DCTSize = 8;
 	private int[,] QuantizationMatrix { get; set; } = GetQuantizationMatrix(CompressionQuality);
+	
+	private static readonly byte[] ZigZagMap = new byte[]
+	{
+		0,  1,  8, 16,  9,  2,  3, 10,
+		17, 24, 32, 25, 18, 11,  4,  5,
+		12, 19, 26, 33, 40, 48, 41, 34,
+		27, 20, 13,  6,  7, 14, 21, 28,
+		35, 42, 49, 56, 57, 50, 43, 36,
+		29, 22, 15, 23, 30, 37, 44, 51,
+		58, 59, 52, 45, 38, 31, 39, 46,
+		53, 60, 61, 54, 47, 55, 62, 63
+	};
 
 	public void Compress(string imagePath, string compressedImagePath)
 	{
@@ -46,8 +58,7 @@ public class JpegProcessor : IJpegProcessor
 				{
 					var subMatrix = GetSubMatrix(bmp, y, DCTSize, x, DCTSize, selector);
 					var channelFreqs = DCT.DCT2D(subMatrix);
-					var quantizedFreqs = Quantize(channelFreqs);
-					var quantizedBytes = ZigZagScan(quantizedFreqs);
+					var quantizedBytes = QuantizeAndZigZagScan(channelFreqs);
 					allQuantizedBytes.AddRange(quantizedBytes);
 				}
 			}
@@ -85,8 +96,8 @@ public class JpegProcessor : IJpegProcessor
 					{
 						var quantizedBytes = new byte[DCTSize * DCTSize];
 						allQuantizedBytes.ReadAsync(quantizedBytes, 0, quantizedBytes.Length).Wait();
-						var quantizedFreqs = ZigZagUnScan(quantizedBytes);
-						var channelFreqs = DeQuantize(quantizedFreqs);
+						var channelFreqs = DequantizeAndZigZagUnScan(quantizedBytes);
+
 						DCT.IDCT2D(channelFreqs, channel);
 					}
 
@@ -127,94 +138,32 @@ public class JpegProcessor : IJpegProcessor
 		return result;
 	}
 
-	private static IEnumerable<byte> ZigZagScan(byte[,] channelFreqs)
+	private byte[] QuantizeAndZigZagScan(double[,] channelFreqs)
 	{
-		return new[]
-		{
-			channelFreqs[0, 0], channelFreqs[0, 1], channelFreqs[1, 0], channelFreqs[2, 0], channelFreqs[1, 1],
-			channelFreqs[0, 2], channelFreqs[0, 3], channelFreqs[1, 2],
-			channelFreqs[2, 1], channelFreqs[3, 0], channelFreqs[4, 0], channelFreqs[3, 1], channelFreqs[2, 2],
-			channelFreqs[1, 3], channelFreqs[0, 4], channelFreqs[0, 5],
-			channelFreqs[1, 4], channelFreqs[2, 3], channelFreqs[3, 2], channelFreqs[4, 1], channelFreqs[5, 0],
-			channelFreqs[6, 0], channelFreqs[5, 1], channelFreqs[4, 2],
-			channelFreqs[3, 3], channelFreqs[2, 4], channelFreqs[1, 5], channelFreqs[0, 6], channelFreqs[0, 7],
-			channelFreqs[1, 6], channelFreqs[2, 5], channelFreqs[3, 4],
-			channelFreqs[4, 3], channelFreqs[5, 2], channelFreqs[6, 1], channelFreqs[7, 0], channelFreqs[7, 1],
-			channelFreqs[6, 2], channelFreqs[5, 3], channelFreqs[4, 4],
-			channelFreqs[3, 5], channelFreqs[2, 6], channelFreqs[1, 7], channelFreqs[2, 7], channelFreqs[3, 6],
-			channelFreqs[4, 5], channelFreqs[5, 4], channelFreqs[6, 3],
-			channelFreqs[7, 2], channelFreqs[7, 3], channelFreqs[6, 4], channelFreqs[5, 5], channelFreqs[4, 6],
-			channelFreqs[3, 7], channelFreqs[4, 7], channelFreqs[5, 6],
-			channelFreqs[6, 5], channelFreqs[7, 4], channelFreqs[7, 5], channelFreqs[6, 6], channelFreqs[5, 7],
-			channelFreqs[6, 7], channelFreqs[7, 6], channelFreqs[7, 7]
-		};
-	}
+		var result = new byte[64];
 
-	private static byte[,] ZigZagUnScan(IReadOnlyList<byte> quantizedBytes)
-	{
-		return new[,]
+		for (int i = 0; i < 64; i++)
 		{
-			{
-				quantizedBytes[0], quantizedBytes[1], quantizedBytes[5], quantizedBytes[6], quantizedBytes[14],
-				quantizedBytes[15], quantizedBytes[27], quantizedBytes[28]
-			},
-			{
-				quantizedBytes[2], quantizedBytes[4], quantizedBytes[7], quantizedBytes[13], quantizedBytes[16],
-				quantizedBytes[26], quantizedBytes[29], quantizedBytes[42]
-			},
-			{
-				quantizedBytes[3], quantizedBytes[8], quantizedBytes[12], quantizedBytes[17], quantizedBytes[25],
-				quantizedBytes[30], quantizedBytes[41], quantizedBytes[43]
-			},
-			{
-				quantizedBytes[9], quantizedBytes[11], quantizedBytes[18], quantizedBytes[24], quantizedBytes[31],
-				quantizedBytes[40], quantizedBytes[44], quantizedBytes[53]
-			},
-			{
-				quantizedBytes[10], quantizedBytes[19], quantizedBytes[23], quantizedBytes[32], quantizedBytes[39],
-				quantizedBytes[45], quantizedBytes[52], quantizedBytes[54]
-			},
-			{
-				quantizedBytes[20], quantizedBytes[22], quantizedBytes[33], quantizedBytes[38], quantizedBytes[46],
-				quantizedBytes[51], quantizedBytes[55], quantizedBytes[60]
-			},
-			{
-				quantizedBytes[21], quantizedBytes[34], quantizedBytes[37], quantizedBytes[47], quantizedBytes[50],
-				quantizedBytes[56], quantizedBytes[59], quantizedBytes[61]
-			},
-			{
-				quantizedBytes[35], quantizedBytes[36], quantizedBytes[48], quantizedBytes[49], quantizedBytes[57],
-				quantizedBytes[58], quantizedBytes[62], quantizedBytes[63]
-			}
-		};
-	}
-
-	private byte[,] Quantize(double[,] channelFreqs)
-	{
-		var result = new byte[channelFreqs.GetLength(0), channelFreqs.GetLength(1)];
-
-		for (int y = 0; y < channelFreqs.GetLength(0); y++)
-		{
-			for (int x = 0; x < channelFreqs.GetLength(1); x++)
-			{
-				result[y, x] = (byte)(channelFreqs[y, x] / QuantizationMatrix[y, x]);
-			}
+			int flatIndex = ZigZagMap[i];
+        
+			int y = flatIndex / 8;
+			int x = flatIndex % 8;
+			result[i] = (byte)(channelFreqs[y, x] / QuantizationMatrix[y, x]);
 		}
 
 		return result;
 	}
-
-	private double[,] DeQuantize(byte[,] quantizedBytes)
+	
+	private double[,] DequantizeAndZigZagUnScan(byte[] quantizedBytes)
 	{
-		var result = new double[quantizedBytes.GetLength(0), quantizedBytes.GetLength(1)];
-		for (int y = 0; y < quantizedBytes.GetLength(0); y++)
+		var result = new double[8,8];
+
+		for (int i = 0; i < 64; i++)
 		{
-			for (int x = 0; x < quantizedBytes.GetLength(1); x++)
-			{
-				result[y, x] =
-					((sbyte)quantizedBytes[y, x]) *
-					QuantizationMatrix[y, x];
-			}
+			int flatIndex = ZigZagMap[i];
+			int y = flatIndex / 8;
+			int x = flatIndex % 8;
+			result[y, x] = ((sbyte)quantizedBytes[i] * QuantizationMatrix[y, x]);
 		}
 
 		return result;
